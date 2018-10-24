47fe5779c472c8d25c09fc981c7cca26122ffe3a
mfr-h2 <mfr-h2@WIN4PC-PC>
2018-10-24 21:42:09 +0300

    add a comment

---
+++ frest APP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frest_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("hzaze");
+            Console.Write("as");
+            Console.ReadKey();
+        }
+    }
+}
